9c8ec428f7a1f3efefca7b046e7b879ac016726f
박응용 <pahkey@gmail.com>
2020-05-07 08:32:53 +0900

    1-04

--- config/settings.py
+++ config/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
