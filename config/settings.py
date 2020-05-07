8a5dc5e8b1fbf46e937a8b20b40b6a4ddece4525
박응용 <pahkey@gmail.com>
2020-05-07 21:19:22 +0900

    4-05

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ls&1oied3t*np!^y6664m!j2-l%ie=8g$w-bjk_pjryzd#3y6b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.78.8.100']
 
 
 # Application definition
