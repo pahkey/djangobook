ab8b282f2a72686f851b598446a89fb9e14c4649
박응용 <pahkey@gmail.com>
2020-05-07 09:04:31 +0900

    2-03

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
