e31bf5a0c3c9866a16358b1135933f086fbc26be
박응용 <pahkey@gmail.com>
2020-05-07 21:51:44 +0900

    4-14

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['52.78.8.100']
+ALLOWED_HOSTS = ['52.78.8.100', 'pybo.kr']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = []
 DEBUG = False
