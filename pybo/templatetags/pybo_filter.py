a909152195b441adfefb3532c4876a04e029338d
박응용 <pahkey@gmail.com>
2020-05-07 15:37:58 +0900

    3-14

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,6 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -6,3 +8,9 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+@register.filter()
+def mark(value):
+    extensions = ["nl2br", "fenced_code"]
+    return mark_safe(markdown.markdown(value, extensions=extensions))
