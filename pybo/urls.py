fe727cea50bed511a7851f56cd7a268fa54c0d77
박응용 <pahkey@gmail.com>
2020-05-07 09:27:06 +0900

    2-05

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:question_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
