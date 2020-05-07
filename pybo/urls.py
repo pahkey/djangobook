adc2a24503cd5d228dbd406995200f4f0307952f
박응용 <pahkey@gmail.com>
2020-05-07 09:19:41 +0900

    2-04

--- pybo/urls.py
+++ pybo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
